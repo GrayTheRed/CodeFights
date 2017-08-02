38759dbacf11113356c1de046026a4f694c0db57
Joshua Gray <j.d.gray@outlook.com>
2017-08-01 18:02:00 -0700

    Completed the Sudoku problem

--- CodeFightsPractice/CodeFightsPractice/Program.cs
+++ CodeFightsPractice/CodeFightsPractice/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeFightsPractice
+﻿namespace CodeFightsPractice
 {
+    using Arrays;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var sudoku = new SudokuProblem();
         }
     }
 }
